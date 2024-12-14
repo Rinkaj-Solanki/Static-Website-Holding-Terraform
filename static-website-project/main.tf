edc7331c4d0a4b33f70b2617432dab5eb13a7839
Rinkaj-Solanki <rinkaj909@gmail.com>
2024-12-14 21:12:12 +0530

    content type for specifying for file content on web

--- static-website-project/main.tf
+++ static-website-project/main.tf
@@ -21,12 +21,14 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.demo-bucket.bucket
   source = "./index.html"
   key    = "index.html"
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "styles_css" {
   bucket = aws_s3_bucket.demo-bucket.bucket
   source = "./styles.css"
   key    = "styles.css"
+  content_type = "text/css"
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
