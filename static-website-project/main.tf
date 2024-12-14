d7e13be765767929b56f63c20a71f176ffa48cd3
Rinkaj-Solanki <rinkaj909@gmail.com>
2024-12-14 21:00:49 +0530

    turning "block public access" to "off" in s3 bucket & adding bucket policy

--- static-website-project/main.tf
+++ static-website-project/main.tf
@@ -28,3 +28,42 @@ resource "aws_s3_object" "styles_css" {
   source = "./styles.css"
   key    = "styles.css"
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.demo-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "mywebapp" {
+  bucket = aws_s3_bucket.demo-bucket.id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Sid = "PublicReadGetObject",
+          Effect = "Allow",
+          Principal = "*",
+          Action = [
+            "s3:GetObject"
+          ],
+          Resource = [
+            "arn:aws:s3:::${aws_s3_bucket.demo-bucket.id}/*"
+          ]
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_s3_bucket_website_configuration" "mywebapp" {
+  bucket = aws_s3_bucket.demo-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
